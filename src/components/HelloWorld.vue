83da668feb55043b013c8309d35f4c4eca305515
lauramog <lmanuela.osorio@udea.edu.co>
2023-01-30 20:40:29 -0500

    message of the email displayed and tailwind installed properly

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,41 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <div class="container">
-    <div class="card">
-      <div class="card-header">GET data</div>
-      <div class="card-body">
-        <div class="input-group input-group-sm">
-          <input type="text" ref="get_term" class="form-control ml-2" placeholder="Title" id="get-data" />
-          <div class="input-group-append">
-            <button class="btn btn-sm btn-primary" @click="getData">Get</button>
+  <div class="flex flex-col justify-center items-center">
+
+    <h1>{{ msg }}</h1>
+    <div class="container">
+      <div class="card">
+        <div class="card-body">
+          <div class="input-group input-group-sm">
+            <input type="text" ref="get_term" class="form-control ml-2" placeholder="Title" id="get-data" />
+            <div class="input-group-append">
+              <button class="btn btn-sm btn-primary" @click="getData">search term</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="container">
-    <h3 class="p-3 text-center"></h3>
-    <table class="table-auto">
-      <thead>
-        <tr>
-          <th>subject</th>
-          <th>From</th>
-          <th>To</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in listItems">
-          <td> {{ item.subject }} </td>
-          <td>{{ item.From }}</td>
-          <td>{{ item.to }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container">
+      <h3 class="p-3 text-center"></h3>
+      <table class="table-fixed">
+        <thead>
+          <tr class="border">
+            <th>subject</th>
+            <th>From</th>
+            <th>To</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border" v-for="item in listItems">
+            <td class="border"> {{ item.Subject }} </td>
+            <td class="border">{{ item.From }}</td>
+            <td class="border">{{ item.To }}</td>
+            <td class="border">{{ item.Message }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
