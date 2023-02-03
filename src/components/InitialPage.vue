58f4cf822dc5d6536ebb17e39b11c3a45e2264dd
lauramog <lmanuela.osorio@udea.edu.co>
2023-02-03 11:24:45 -0500

    read all button implemented for all messages

--- src/components/InitialPage.vue
+++ src/components/InitialPage.vue
@@ -8,7 +8,7 @@
           <div class="input-group input-group-sm">
             <input type="text" ref="get_term" class="form-control ml-2" placeholder="Title" id="get-data" />
             <div class="input-group-append">
-              <button class="btn btn-sm btn-primary" @click="getData">search term</button>
+              <button @click="getData">search term</button>
             </div>
           </div>
         </div>
@@ -17,7 +17,7 @@
 
     <div class="container">
       <h3 class="p-3 text-center"></h3>
-      <table class="table-fixed">
+      <table class="table-fixed ">
         <thead>
           <tr class="border">
             <th>subject</th>
@@ -31,7 +31,16 @@
             <td class="border"> {{ item.Subject }} </td>
             <td class="border">{{ item.From }}</td>
             <td class="border">{{ item.To }}</td>
-            <td class="border">{{ item.Message }}</td>
+
+            <p class="p" v-if="!readActivated">{{ item.Message.slice(0, 100) }}
+            <div class="flex space-x-2 justify-center"></div>
+            <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light"
+              class="inline-block px-6 py-2.5  bg-teal-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              v-if="!readActivated" @click="activateReadMore">...Read all </button>
+            </p>
+            <p class="p" v-if="readActivated">{{ item.Message }}</p>
+            <div class="flex space-x-2 justify-center"></div>
+
           </tr>
         </tbody>
       </table>
@@ -40,7 +49,7 @@
 </template>
 
 <script>
-const baseURL = "http://localhost:8081";
+const baseURL = "http://localhost:8082";
 
 export default {
   name: 'InitialPage',
@@ -48,11 +57,16 @@ export default {
     msg: String
   },
   data() {
+
     return {
-      listItems: []
+      listItems: [],
+      readActivated: false
+
+
     }
   },
   methods: {
+
     async getData() {
       const term = this.$refs.get_term.value;
       if (term !== "") {
@@ -62,8 +76,13 @@ export default {
         const finalRes = await res.json();
         this.listItems = finalRes;
       }
+    },
+
+    activateReadMore() {
+      this.readActivated = true;
     }
   },
+
   mounted() {
     this.getData()
   }
